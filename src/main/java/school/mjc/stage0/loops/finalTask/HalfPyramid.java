64a66eae1519dab18459517ef7f736dcc08ff26c
faridabderrahmani <abderrahmani@yandex.ru>
2024-01-02 20:51:33 +0100

    1st try

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,10 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for(int i = 1; i <= cathetusLength/2+1; i++){
+            for(int j = i; j <= cathetusLength/2+1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
+
         new HalfPyramid().printHalfPyramid(8);
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,10 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int result = numberToPrint;
+        for(int i = 1; i < power; i++){
+            result *= numberToPrint;
+        }
+        System.out.println(result);
 
     }
 
     public static void main(String[] args) {
+
         new Power().printPower(10, 3);
     }
 }
